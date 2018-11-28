d44e8f54449b150c4828e720494d2c70d08696b3
Saahil Ali <programmer290399@gmail.com>
2018-11-28 19:32:20 +0530

    errror in iterating

--- Levenshtein-cosine-similarity.py
+++ Levenshtein-cosine-similarity.py
@@ -9,19 +9,29 @@ import itertools
 from read_data import read_JSON
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
-
-
-dishes = read_JSON('zomato_menu.json')
-
-
-
-
-
+import multiprocessing
+import threading 
 
 stop = stopwords.words('english')
-
 WORD = re.compile(r'\w+')
 stemmer = PorterStemmer()
+
+
+def create_chunks(bulk_data, cpu_count , num_of_dishes ) :
+    total_data_length = len(bulk_data)
+    len_of_chunks = int(total_data_length/(cpu_count * 2 ))
+    
+    chunks = list()
+    for i in range(0, total_data_length , len_of_chunks):
+        chunks.append(bulk_data[i:i + len_of_chunks])
+    return chunks
+
+def get_file_names(n) :
+    names = list()
+    for i in range(0,n) :
+        names.append('similarity_index_temp_file-' + str(i) + '.json')
+    return names
+
 
 def get_cosine(vec1, vec2):
     # print vec1, vec2
@@ -77,40 +87,90 @@ def levenshtein(dish_A, dish_B):
         return score 
 
 
-test_cases = ['Indian Chilly Burger', 'Onion Pakoda', 'Tandoori Maggi', 'Mini Barbeque Sandwich', 'Manchurian with Chowmein','Potato Chaat','Mysore Dosa', 'Crispy Corn', 'Sev Paneer', 'Hari Bhari Sabji', 'Shrikhand','Sabudana Khichadi']
-crit_list = [0.5, 0.6, 0.7, 0.8, 0.9]
-for case in  itertools.product(test_cases,crit_list) :
-    out_file = open('similarity_index_new21.json' , 'a+' , encoding='utf-8')
-    out_file.write('------------'+str(case[0])+':'+str(case[1])+'------------')
-    out_file.write('\n')
-    out_file.close()
-    main = dict()
-    print('\n Generating index in memory , please wait .....')
-    bar = IncrementalBar('Loading', max= 93655)
-    for dish in dishes :
-        if  not main.get(dish,False) :
-            if dish == case[0]:
-                main[dish] = list()
-                bar.next()
-    bar.finish()
-    bar = IncrementalBar('Processing', max= pow(93655,2))
+def get_crit_list(dish_name , freq_dist):
+    freq = int(freq_dist[dish_name])
+    if freq >= 100 : crit_list = [0.8,0.9]
+    elif freq < 100 and freq >= 50 : crit_list = [0.7,0.8]
+    else : crit_list = [0.6,0.7]
+    return crit_list
+
+def create_test_cases(test_cases, f_name , dishes ):
+    crit_data = dict()
+    freq_dist = json.loads(open('dish_freq.json').read())
+    for dish in test_cases :
+        crit_data[dish] = get_crit_list(dish,freq_dist)
+    cases = list()
+    print(crit_data)
     
-    for dish  in dishes :
-        score = (0.5 * get_similarity(case[0],dish)) + (0.5 * levenshtein(case[0],dish))
-        if  score > case[1] : 
-                if dish not in main[case[0]] : main[case[0]].append(dish) 
-        
+    for key,value in  crit_data.items():
+        cases.append(list(itertools.product(key,value)))
+    print(cases)
+    
+    bar = IncrementalBar('Processing', max= len(list(cases))) 
+    for i in range(0,len(cases)) :
+        for case in cases[i] :
+            out_file = open(f_name , 'a+' , encoding='utf-8')
+            out_file.write('\n')
+            out_file.write('------------'+str(case[0])+':'+str(case[1])+'------------')
+            out_file.write('\n')
+            out_file.close()
+            main = dict()
+            print('\n Generating index in memory , please wait .....')
+            for dish in dishes :
+                if  not main.get(dish,False) :
+                    if dish == case[0]:
+                        main[dish] = list()
             
-            
+
+            for dish  in dishes :
+                score = (0.5 * get_similarity(case[0],dish)) + (0.5 * levenshtein(case[0],dish))
+                if  score > case[1] : 
+                        if dish not in main[case[0]] : main[case[0]].append(dish) 
+                
+
+            out_file = open(f_name , 'a+' , encoding='utf-8')
+            out_file.write('\n')
+            json.dump(main,out_file)
+            out_file.write('\n')
+            out_file.close()    
         bar.next()
-    out_file = open('similarity_index_new21.json' , 'a+' , encoding='utf-8')
-    out_file.write('\n')
-    json.dump(main,out_file)
-    out_file.write('\n')
-    out_file.close()    
         
 
+if __name__=='__main__':
+    try :
+        cpu_count = multiprocessing.cpu_count()
+    except :
+        cpu_count = 4 
+    dishes = read_JSON('dish_freq.json')
+
+    freq_dist = json.loads(open('dish_freq.json').read())
+
+    num_of_dishes = int(input("Enter number of dishes to process"))
+
+    dishes_to_process = dishes[:num_of_dishes]
+    
+    chunks = create_chunks(dishes_to_process,cpu_count,num_of_dishes)
+
+    filenames = get_file_names(len(chunks))
+    processes = list()
+    for i in range(0,len(chunks)):
+        processes.append(multiprocessing.Process(target=create_test_cases , args=(chunks[i] , filenames[i] , dishes )))
+    for i in range(0,(len(processes)- cpu_count + 1) , cpu_count):
+        rng = list(range(0,cpu_count))
+        for j in rng :
+            processes[j].start()
+        for j in rng :
+            processes[j].join()
+    main_file = open('main.json', 'a+' , encoding='utf-8')
+    for file_name in filenames :
+        main_file.write('\n')
+        data = json.loads(open(file_name).read())
+        json.dump(data , main_file)
+    main_file.close()
 
 
-print('Done!')
+
+
+
+    print('\n Done!')
 
--- read_data.py
+++ read_data.py
@@ -1,33 +1,9 @@
 import json 
-from progress.bar import IncrementalBar
-
-
 
 def read_JSON(name):
-
-       bar = IncrementalBar('Loading Data:', max = 93655 )
-
-       json_data=open(name).read()
-
-       data = json.loads(json_data)
-
-       mappings  = data['foo']
-
-       dishes = list()
-
-       for element in mappings :
-              dish_list = element["dish_mappings"]
-              for dish in dish_list :
-                     dishes.append(dish["dish_name"])
-                     bar.next()
-       bar.finish()
-       return dishes 
-
-
+       return list(json.loads(open(name).read()).keys())
 
 
 if __name__ == '__main__' :
-   lst = read_JSON('zomato_menu.json')
-   for e in lst :
-       print(e)
-       
+       lst = read_JSON('dish_freq.json')
+       print(len(lst))
